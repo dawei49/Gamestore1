ebfc0b99534d4451e4b70aaa006b0ac81c3e4cfc
dawei49 <daweihan1996@gmail.com>
2021-12-15 18:09:21 +1100

    first commit

---
+++ Config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBFjabl9JWaBj_AItmq9Oa5YI-WroAVRLU",
+  authDomain: "rnfb2-9893f.firebaseapp.com",
+  projectId: "rnfb2-9893f",
+  storageBucket: "rnfb2-9893f.appspot.com",
+  messagingSenderId: "501288828614",
+  appId: "1:501288828614:web:b3512393e17e069457e1c7",
+  measurementId: "G-QN09RRJTR3"
+}
